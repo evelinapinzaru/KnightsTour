a1434590bf4d6b71631df8f3a82397cd09f6fd28
Evelina Pînzaru <evelinapinzaru2003@gmail.com>
2024-01-12 20:33:41 +0200

    Finish Dockerfile, update everything else
    In this working session, I've accomplished the following:
    1. developed a functional Dockerfile;
    2. enhanced code readability & clarity, by renaming variables, adding explanatory comments to each function, and restructuring the code;
    3. made the on-screen output more user-friendly & visually pleasing by illustrating the chessboard and playing around with ANSI colour codes.

--- input.h
+++ input.h
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 
-#ifndef input
-#define input
+#ifndef INPUT
+#define INPUT
 enum option { SIZE, POSITION };
 #endif
 
+// this function reads and validates user input
 int read_var(char* text, enum option opt, int set_value) {
 	char buf[20]; buf[19] = '\n';
 	int var = -1;
@@ -17,10 +16,9 @@ int read_var(char* text, enum option opt, int set_value) {
 		do {
 			printf(text);
 			fgets(buf, sizeof(buf), stdin);
-			sscanf_s(buf, "%d", &var);
+			if (sscanf(buf, "%d", &var) == EOF) printf("\nReading N error!\n\n");
 			if (var > 0 && var <= set_value)
-				printf("\nThere are no solutions for a %dx%d board!\n", var, var);
-			printf("\n");
+				printf("\n\x1b[2;31mThere are no solutions for a %dx%d board! Try again ...\x1b[0m\n\n", var, var);
 		} while (var <= set_value);
 		break;
 
@@ -28,10 +26,11 @@ int read_var(char* text, enum option opt, int set_value) {
 		do {
 			printf(text);
 			fgets(buf, sizeof(buf), stdin);
-			sscanf_s(buf, "%d", &var);
-			printf("\n");
+			if (sscanf(buf, "%d", &var) == EOF) printf("\nReading coordinates error!\n\n");
+			if (var < 0 || var >= set_value) printf("\n\x1b[2;31mIncorrect coordinate! Try again ...\x1b[0m\n\n");
 		} while (var < 0 || var >= set_value);
 		break;
 	}
+	printf("\n");
 	return var;
 }
